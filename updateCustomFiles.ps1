519372ea3f5c079481ee5b52d372c27e8228e563
Kevin Eifinger <k.eifinger@googlemail.com>
2019-03-21 23:16:28 +0100

    updateCustomCards

---
+++ updateCustomFiles.ps1
@@ -0,0 +1,7 @@
+Invoke-WebRequest https://raw.githubusercontent.com/thomasloven/lovelace-fold-entity-row/master/fold-entity-row.js -OutFile .\www\fold-entity-row.js
+Invoke-WebRequest https://raw.githubusercontent.com/thomasloven/lovelace-card-tools/master/card-tools.js -OutFile .\www\card-tools.js
+Invoke-WebRequest https://raw.githubusercontent.com/thomasloven/lovelace-slider-entity-row/master/slider-entity-row.js -OutFile .\www\slider-entity-row.js
+Invoke-WebRequest https://raw.githubusercontent.com/thomasloven/lovelace-state-switch/master/state-switch.js -OutFile .\www\state-switch.js
+Invoke-WebRequest https://raw.githubusercontent.com/thomasloven/lovelace-auto-entities/master/auto-entities.js -OutFile .\www\auto-entities.js
+Invoke-WebRequest https://raw.githubusercontent.com/ciotlosm/custom-lovelace/master/plan-coordinates/plan-coordinates.js -OutFile .\www\custom-lovelace\plan-coordinates\plan-coordinates.js
+Invoke-WebRequest https://raw.githubusercontent.com/custom-cards/config-template-card/master/dist/config-template-card.js -OutFile .\www\config-template-card.js
--- www/state-switch.js
+++ www/state-switch.js
@@ -1,50 +1,82 @@
-class StateSwitch extends Polymer.Element{
+customElements.whenDefined('card-tools').then(() => {
+const cardTools = customElements.get('card-tools');
+class StateSwitch extends cardTools.LitElement {
 
-    setConfig(config) {
-      this.config = config;
-  
-      this.root = document.createElement('div');
-      this.appendChild(this.root);
-  
-      this.cardSize = 1;
-      this.cards = {}
-  
-      for(var k in this.config.states) {
-        const conf = this.config.states[k];
-        let tag = conf.type;
-        tag = tag.startsWith("custom:")? tag.substr(7) : `hui-${tag}-card`;
-  
-        const card = this.cards[k] = document.createElement(tag);
-        card.setConfig(conf);
-  
-        this.cardSize = Math.max(this.cardSize, card.getCardSize());
-      }
+  setConfig(config) {
+    cardTools.checkVersion(0.4);
+    this.config = config;
+
+    this.cardSize = 1;
+    this.cards = {}
+
+    for(var k in this.config.states) {
+      this.cards[k] = cardTools.createCard(this.config.states[k]);
+      this.cardSize = Math.max(this.cardSize, this.cards[k].getCardSize());
     }
-  
-    set hass(hass) {
-      if(!hass) return;
-  
-      const lastCard = this.currentCard;
-      if (this.config.entity === 'user') {
-        this.currentCard = this.cards[hass.user.name] || this.cards[this.config.default];
-      } else {
-        let state = hass.states[this.config.entity];
-        this.currentCard = ((state)?this.cards[state.state]:null) || this.cards[this.config.default];
-      }
-  
-      if(this.currentCard != lastCard) {
-        while(this.root.firstChild) this.root.removeChild(this.root.firstChild);
-        this.root.appendChild(this.currentCard);
-      }
-  
-      for(var k in this.cards)
-        this.cards[k].hass = hass;
+
+    this.idCard = cardTools.createCard({
+      type: "markdown",
+      title: "Device ID",
+      content: `Your device id is: \`${cardTools.deviceID}\``,
+    });
+
+    if(config.entity === 'hash') {
+      window.addEventListener("location-changed", () => this.updateCard());
     }
-  
-    getCardSize() {
-      return this.cardSize;
-    }
-  
   }
-  
-  customElements.define('state-switch', StateSwitch);
+
+  render() {
+    return cardTools.LitHtml`
+    <div id="root">${this.currentCard}</div>
+    `;
+  }
+
+  updateCard() {
+    const hass = this._hass;
+    if(!hass) return;
+    const lastCard = this.currentCard;
+    if (this.config.entity === 'user') {
+      this.currentCard = this.cards[hass.user.name]
+        || this.cards[this.config.default];
+    } else if(this.config.entity == 'browser') {
+      this.currentCard = this.cards[cardTools.deviceID]
+        || ((this.config.default)
+          ? this.cards[this.config.default]
+          : this.idCard);
+    } else if(this.config.entity == 'hash') {
+        this.currentCard = this.cards[location.hash.substr(1)]
+          || this.cards[this.config.default];
+    } else {
+      let state = hass.states[this.config.entity];
+      this.currentCard = ((state)?this.cards[state.state]:null)
+        || this.cards[this.config.default];
+    }
+
+    if(this.currentCard != lastCard) this.requestUpdate();
+  }
+
+  set hass(hass) {
+    if(!hass) return;
+    this._hass = hass;
+
+    this.updateCard();
+
+    for(var k in this.cards)
+      this.cards[k].hass = hass;
+  }
+
+  getCardSize() {
+    return this.cardSize;
+  }
+
+}
+
+customElements.define('state-switch', StateSwitch);
+});
+
+setTimeout(() => {
+  if(customElements.get('card-tools')) return;
+  customElements.define('state-switch', class extends HTMLElement{
+    setConfig() { throw new Error("Can't find card-tools. See https://github.com/thomasloven/lovelace-card-tools");}
+  });
+}, 2000);

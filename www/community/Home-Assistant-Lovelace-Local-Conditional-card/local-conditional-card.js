f8ebb286aa5086f8fd1a3e6f2583976ac4fe8134
eifinger <k.eifinger@googlemail.com>
2020-03-22 16:50:07 +0100

    local-conditional-card 1.1.1

--- www/community/Home-Assistant-Lovelace-Local-Conditional-card/local-conditional-card.js
+++ www/community/Home-Assistant-Lovelace-Local-Conditional-card/local-conditional-card.js
@@ -20,26 +20,42 @@ class LocalConditionalCard extends LitElement {
         const thisDomain = "local_conditional_card";
         if (!this._config || !hass) return;
         this._hass = hass;
+        if (this._card) {
+            this._card.hass = hass;
+        }
         if (this.hassPatched) return;
         const callService = hass.callService;
         hass.callService = (domain, service, serviceData) => {
-            const methods = ["toggle", "show", "hide"];
-            if (domain === thisDomain && serviceData && serviceData.id === this._config.id && methods.includes(service)) {
+            const methods = ["toggle", "show", "hide", "set"];
+            const getId = a => {
+                if (typeof a === "object")
+                    return Object.keys(a);
+                return [a];
+            };
+            if (domain === thisDomain && methods.includes(service) && serviceData && serviceData.ids
+                && serviceData.ids.flatMap(getId).includes(this._config.id)) {
+                const ids = serviceData.ids.flatMap(getId);
                 if (service === "toggle")
                     this._show = !this._show;
                 if (service === "show")
                     this._show = true;
                 if (service === "hide")
                     this._show = false;
+                if (service === "set") {
+                    this._show = serviceData.ids[ids.indexOf(this._config.id)][this._config.id];
+                }
                 this.requestUpdate();
-                return Promise.resolve();
+                let new_ids = serviceData.ids.filter(ido => getId(ido)[0] !== this._config.id);
+                if (new_ids.length === 0)
+                    return Promise.resolve();
+                serviceData.ids = new_ids;
+                return callService(domain, service, serviceData);
             }
             return callService(domain, service, serviceData);
         };
 
         this.hassPatched = true;
         document.querySelector("home-assistant").hassChanged(hass, hass);
-        this._card.hass = hass;
     }
 
     shouldUpdate(changedProps) {
@@ -54,7 +70,7 @@ class LocalConditionalCard extends LitElement {
             throw new Error("You need to define 'card' in your configuration.");
         }
         this._config = config;
-        this._card = this.createCard(config.card);
+        this.createCard(config.card);
         this._show = config.default === "show";
     }
 
@@ -64,19 +80,34 @@ class LocalConditionalCard extends LitElement {
             tag = tag.substr("custom:".length);
         else
             tag = `hui-${tag}-card`;
-        let element = document.createElement(tag);
+
         try {
-            element.setConfig(config);
+            if (!customElements.get(tag)) {
+                customElements.whenDefined(tag).then(() => {
+                    let element = document.createElement(tag);
+                    element.setConfig(config);
+                    element.hass = this._hass;
+                    this._card = element;
+                });
+            } else {
+                let element = document.createElement(tag);
+                element.setConfig(config);
+                element.hass = this._hass;
+                this._card = element;
+            }
         } catch (err) {
             console.error(tag, err);
-            return this.createCard({type: "error", error: err.message, origConfig: this._config});
+            this._card = this.createCard({type: "error", error: err.message, origConfig: this._config});
         }
-        return element;
     }
 
     render() {
-        if (this._show === true || this._card.localName === "hui-error-card")
+        const visible = this._show || this._card && this._card.localName === "hui-error-card";
+        this.style.setProperty("display", visible ? "" : "none");
+        this.style.setProperty("margin", "0");
+        if (visible) {
             return html`${this._card}`;
+        }
         return html``;
     }
 }

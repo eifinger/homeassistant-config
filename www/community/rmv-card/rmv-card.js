a3e32ac53b52dcbcb753f927c2f840ee5db4bcca
eifinger <k.eifinger@googlemail.com>
2020-07-12 10:28:14 +0200

    update deps

--- www/community/rmv-card/rmv-card.js
+++ www/community/rmv-card/rmv-card.js
@@ -1,68 +1,68 @@
 class RmvCard extends HTMLElement {
-  set hass (hass) {
+  set hass(hass) {
     const entityId = this.config.entity
     const state = hass.states[entityId]
-    const name = state.attributes['friendly_name']
+    if (this.config.friendly_name) { var name = this.config.friendly_name }
+    else { var name = state.attributes['friendly_name'] }
 
     if (!this.content) {
       const card = document.createElement('ha-card')
-      card.header = name
+      if (!this.config.hide_title) { card.header = name }
       this.content = document.createElement('div')
       const style = document.createElement('style')
       style.textContent = `
-        table {
-          width: 100%;
-          padding: 6px 14px;
-        }
-        td {
-          padding: 3px 0px;
-        }
-        td.shrink {
-          white-space:nowrap;
-        }
-        td.expand {
-          width: 99%
-        }
-        span.line {
-          font-weight: bold;
-          font-size:0.9em;
-          padding:3px 8px 2px 8px;
-          color: #fff;
-          background-color: #888;
-          margin-right:0.7em;
-        }
-        span.S {
-          background-color: #009252;
-        }
-        span.U-Bahn {
-          background-color: #0067ad;
-        }
-        span.Tram {
-          background-color: #eb6810;
-        }
-        span.Bus {
-          background-color: #a3047a;
-        }
-        span.Bahn {
-          background-color: #000000;
-        }
-        `
+      table {
+        width: 100%;
+        padding: 6px 14px;
+      }
+      td {
+        padding: 3px 0px;
+      }
+      td.shrink {
+        white-space:nowrap;
+      }
+      td.expand {
+        width: 99%
+      }
+      span.line {
+        font-weight: bold;
+        font-size:0.9em;
+        padding:3px 8px 2px 8px;
+        color: #fff;
+        background-color: #888;
+        margin-right:0.7em;
+      }
+      span.S {
+        background-color: #009252;
+      }
+      span.U-Bahn {
+        background-color: #0067ad;
+      }
+      span.Tram {
+        background-color: #eb6810;
+      }
+      span.Bus {
+        background-color: #a3047a;
+      }
+      span.Bahn {
+        background-color: #000000;
+      }
+      `
       card.appendChild(style)
       card.appendChild(this.content)
       this.appendChild(card)
     }
 
     var tablehtml = `
-    <table>
-    `
-    const now = new Date()
-    const utc = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(),
-      now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds())
+  <table>
+  `
+
     const next = {
       'line': state.attributes['line'],
       'product': state.attributes['product'],
       'destination': state.attributes['destination'],
       'departure_time': state.attributes['departure_time'],
+      'minutes': state.attributes['minutes'],
       'direction': state.attributes['direction']
     }
     const journeys = [next].concat(state.attributes['next_departures'])
@@ -75,31 +75,36 @@ class RmvCard extends HTMLElement {
       const product = journey['product']
 
       const jtime = new Date(journey['departure_time'] + 'Z')
-      const time = parseInt((jtime.getTime() - utc) / 60000)
+      const time = jtime.toISOString().substr(11, 5)
+      const minutes = parseInt(journey['minutes'])
 
       tablehtml += `
-          <tr>
-            <td class="shrink" style="text-align:center;"><span class="line ${product} ${linename}">${linename}</span></td>
-            <td class="expand">${destination}</td>
-            <td class="shrink" style="text-align:right;">${time}</td>
-          </tr>
+        <tr>
+          <td class="shrink" style="text-align:center;"><span class="line ${product} ${linename}">${linename}</span></td>
+          <td class="expand">${destination}</td>
       `
+      tablehtml += `          <td class="shrink" style="text-align:right;">`
+      if (!this.config.hide_minutes) { tablehtml += `${minutes}` }
+      if (this.config.show_time) { tablehtml += ` <small>(${time})</small>` }
+      tablehtml += `</td>`
+
+      tablehtml += `        </tr>`
     }
     tablehtml += `
-    </table>
-    `
+  </table>
+  `
 
     this.content.innerHTML = tablehtml
   }
 
-  setConfig (config) {
+  setConfig(config) {
     if (!config.entity) {
       throw new Error('You need to define an entity')
     }
     this.config = config
   }
 
-  getCardSize () {
+  getCardSize() {
     return 1
   }
 }

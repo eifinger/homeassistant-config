c6ea95003455d495cc40a345ab595fce30284730
eifinger <k.eifinger@googlemail.com>
2020-08-01 17:23:18 +0200

    update to 0.113.3

--- changeHomeassistantVersion.sh
+++ changeHomeassistantVersion.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-[[ -z "$1" ]] && { echo "Please supply current version"; exit 1; }
-[[ -z "$2" ]] && { echo "Please supply target version"; exit 1; }
-sed -i 's/'"$1"'/'"$2"'/g' docker-compose.yaml
-sed -i 's/'"$1"'/'"$2"'/g' .github/workflows/homeassistant-installed.yaml
-sed -i 's/'"$1"'/'"$2"'/g' README.md
-sed -i 's/'"$1"'/'"$2"'/g' www/plantuml/homeassistant-architecture.puml
+[[ "$#" -eq "0" ]] && { echo "Please supply target version"; exit 1; }
+[[ "$#" -eq "1" ]] && { currentVersion=$(cat .HA_VERSION); targetVersion="$1"; }
+[[ "$#" -eq "2" ]] && { currentVersion="$1"; targetVersion="$2"; }
+sed -i 's/'"$currentVersion"'/'"$targetVerison"'/g' docker-compose.yaml .github/workflows/homeassistant-installed.yaml README.md www/plantuml/homeassistant-architecture.puml
+echo "Changed Homeassistant version from $currentVersion to $targetVersion in all files."

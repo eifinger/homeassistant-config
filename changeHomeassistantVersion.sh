9ce2579b541034660b43b8d815b685fb9f013084
Kevin Eifinger <k.eifinger@googlemail.com>
2020-10-03 15:34:38 +0200

    fix typo in changeHomeassistantVersion.sh

--- changeHomeassistantVersion.sh
+++ changeHomeassistantVersion.sh
@@ -2,5 +2,5 @@
 [[ "$#" -eq "0" ]] && { echo "Please supply target version"; exit 1; }
 [[ "$#" -eq "1" ]] && { currentVersion=$(cat .HA_VERSION); targetVersion="$1"; }
 [[ "$#" -eq "2" ]] && { currentVersion="$1"; targetVersion="$2"; }
-sed -i 's/'"$currentVersion"'/'"$targetVerison"'/g' docker-compose.yaml .github/workflows/homeassistant-installed.yaml README.md www/plantuml/homeassistant-architecture.puml
+sed -i 's/'"$currentVersion"'/'"$targetVersion"'/g' docker-compose.yaml .github/workflows/homeassistant-installed.yaml README.md www/plantuml/homeassistant-architecture.puml
 echo "Changed Homeassistant version from $currentVersion to $targetVersion in all files."
